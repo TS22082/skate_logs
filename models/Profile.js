6243f03b650721c43d5d033741c4b06e688125b7
GeekCentric <ts22082@gmail.com>
2019-02-16 15:18:16 -0800

    remove education and experience from models

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const ProfileSchema = new Schema({
   user: {
@@ -34,65 +34,7 @@ const ProfileSchema = new Schema({
   githubusername: {
     type: String
   },
-  experience: [
-    {
-      title: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
-  education: [
-    {
-      school: {
-        type: String,
-        required: true
-      },
-      degree: {
-        type: String,
-        required: true
-      },
-      fieldofstudy: {
-        type: String,
-        required: true
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
+
   social: {
     youtube: {
       type: String
@@ -114,6 +56,6 @@ const ProfileSchema = new Schema({
     type: Date,
     default: Date.now
   }
-});
+})
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
