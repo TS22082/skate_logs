d77db4380f91fd7dd12d05c9dbd00d2ba76d1e21
GeekCentric <ts22082@gmail.com>
2019-02-17 14:49:19 -0800

    create registerUser logic, install axios

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { GET_ERRORS } from './types'
+
+//Register User
+export const registerUser = (userData, history) => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatchEvent({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}
