3a2001ba6cc0b40f0dc811014f05f6158f0c2068
GeekCentric <ts22082@gmail.com>
2019-02-20 10:48:38 -0800

    make animated greyscale background for landing

--- client/src/components/layout/landing/Landing.js
+++ client/src/components/layout/landing/Landing.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import video from '../../../img/Cross_frame.mp4'
+import './landing.css'
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="bg-video">
+        <video src={video} className="bg-video__content" autoPlay loop />
       </div>
     )
   }
