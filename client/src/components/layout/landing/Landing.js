2e5dd60333dee8f692ac0b11afff8cb3ad1ac7e3
GeekCentric <ts22082@gmail.com>
2019-02-21 13:04:07 -0800

    add login section to landing

--- client/src/components/layout/landing/Landing.js
+++ client/src/components/layout/landing/Landing.js
@@ -5,8 +5,13 @@ import './landing.css'
 class Landing extends Component {
   render() {
     return (
-      <div className="bg-video">
-        <video src={video} className="bg-video__content" autoPlay loop />
+      <div className="landing">
+        <div className="bg-video">
+          <video src={video} className="bg-video__content" autoPlay loop />
+        </div>
+        <div className="login_and_singup">
+          <h2 class="landing-heading">Skate about it</h2>
+        </div>
       </div>
     )
   }
