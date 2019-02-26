f2d64ca3e9d48d70e26c5ca3656f715a5ca11790
GeekCentric <ts22082@gmail.com>
2019-02-26 09:44:51 -0800

    create client auth components signup, login, logout

---
+++ client/src/components/auth/signin/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'signin.css'
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign in works</h1>
+      </div>
+    )
+  }
+}
+
+export default SignIn
---
+++ client/src/components/auth/signout/SignOut.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'signout.css'
+
+class SignOut extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Signout works</h1>
+      </div>
+    )
+  }
+}
+
+export default SignOut
