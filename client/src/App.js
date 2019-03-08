841d9bc1dca2d01d639c1555d1a736908c765f75
GeekCentric <ts22082@gmail.com>
2019-03-08 10:49:57 -0800

    move login and signup to landing component

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Landing from './components/layout/landing/Landing'
 import './App.css'
 import Nav from './components/nav/Nav'
-import Login from './components/auth/login/Login'
-import Signup from './components/auth/signup/SignUp'
 
 import store from './redux_state/store'
 import { Provider } from 'react-redux'
@@ -17,8 +15,6 @@ class App extends Component {
           <div className="App">
             <Nav />
             <Route exact path="/" component={Landing} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
           </div>
         </Router>
       </Provider>
