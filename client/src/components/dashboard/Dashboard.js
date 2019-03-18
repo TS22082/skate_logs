b483de79c1e1783265e2d1487a27d236fc054d42
GeekCentric <ts22082@gmail.com>
2019-03-18 11:03:26 -0700

    retrieve longitude and latidude

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
+import { geolocated } from 'react-geolocated'
 import Nav from '../nav/Nav'
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      longitude: '',
+      latitude: ''
+    }
+  }
+
   render() {
-    return (
+    return !this.props.isGeolocationAvailable ? (
+      <div>Your browser does not support Geolocation</div>
+    ) : !this.props.isGeolocationEnabled ? (
+      <div>Geolocation is not enabled</div>
+    ) : this.props.coords ? (
       <div>
         <Nav />
-        <h1>Dashboard works</h1>
+        <p>latitude: {this.props.coords.latitude}</p>
+        <p>longitude: {this.props.coords.longitude}</p>
       </div>
+    ) : (
+      <div>Getting the location data&hellip; </div>
     )
   }
 }
 
-export default Dashboard
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false
+  },
+  userDecisionTimeout: 5000
+})(Dashboard)
