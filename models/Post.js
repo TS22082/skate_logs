203971e8f98436abce161e608f5d7e2c7587c063
GeekCentric <ts22082@gmail.com>
2019-03-28 09:29:36 -0700

    add places for likes, comments, and save original skate spot posters id

--- models/Post.js
+++ models/Post.js
@@ -23,6 +23,30 @@ const PostSchema = new Schema({
   latitude: {
     type: String
   },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
   date: {
     type: Date,
     default: Date.now
