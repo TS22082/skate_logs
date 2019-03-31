1932da3e34cc3ce2110c93d85fb11880a6c3d279
GeekCentric <ts22082@gmail.com>
2019-03-30 17:14:33 -0700

    custom google map styling

--- client/src/components/dashboard/map/Map.js
+++ client/src/components/dashboard/map/Map.js
@@ -17,6 +17,363 @@ class Map extends Component {
       lat: this.props.latitude,
       lng: this.props.longitude
     }
+
+    const mapOptions = {
+      styles: [
+        {
+          featureType: 'administrative',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'landscape',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'landscape.man_made',
+          elementType: 'geometry',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              saturation: '-72'
+            },
+            {
+              gamma: '0.44'
+            }
+          ]
+        },
+        {
+          featureType: 'landscape.man_made',
+          elementType: 'labels.text',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'off'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.attraction',
+          elementType: 'labels.text',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.attraction',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.government',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              color: '#3d2a2a'
+            },
+            {
+              weight: '0.04'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.place_of_worship',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.school',
+          elementType: 'geometry',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'poi.school',
+          elementType: 'labels.text',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'road',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'road.arterial',
+          elementType: 'geometry.fill',
+          stylers: [
+            {
+              saturation: '61'
+            }
+          ]
+        },
+        {
+          featureType: 'road.local',
+          elementType: 'geometry',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'labels',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.bus',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.bus',
+          elementType: 'labels.text',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.bus',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.bus',
+          elementType: 'labels.text.stroke',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.bus',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              saturation: '51'
+            },
+            {
+              hue: '#ff0000'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.rail',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'simplified'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.rail',
+          elementType: 'geometry.fill',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              color: '#a00c0c'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.rail',
+          elementType: 'geometry.stroke',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              color: '#730000'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.rail',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              color: '#000000'
+            }
+          ]
+        },
+        {
+          featureType: 'transit.station.rail',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'on'
+            }
+          ]
+        },
+        {
+          featureType: 'water',
+          elementType: 'all',
+          stylers: [
+            {
+              visibility: 'on'
+            },
+            {
+              saturation: '-84'
+            },
+            {
+              lightness: '70'
+            },
+            {
+              color: '#ad2828'
+            }
+          ]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#4284aa'
+            }
+          ]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry.fill',
+          stylers: [
+            {
+              saturation: '-61'
+            }
+          ]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              visibility: 'off'
+            }
+          ]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [
+            {
+              visibility: 'off'
+            }
+          ]
+        }
+      ]
+    }
+
     return (
       <div>
         <div style={{ height: '100vh', width: '100%' }}>
@@ -27,6 +384,7 @@ class Map extends Component {
             }}
             defaultCenter={center}
             defaultZoom={14}
+            options={mapOptions}
           >
             <UserLocation
               lat={this.props.latitude}
@@ -36,7 +394,6 @@ class Map extends Component {
             {this.props.skateSpots.posts.map(skatespot => (
               <SkateSpot
                 key={skatespot._id}
-                // id={skatespot._id}
                 skatespot={skatespot}
                 lat={skatespot.latitude}
                 lng={skatespot.longitude}
