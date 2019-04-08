2ee19af0d53ef971d83cad622eb823d64c1bd00e
GeekCentric <ts22082@gmail.com>
2019-04-08 11:49:27 -0700

    add icons for skate spots

--- client/src/components/dashboard/map/skate_spot/SkateSpot.js
+++ client/src/components/dashboard/map/skate_spot/SkateSpot.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './skateSpot.css'
+import sk8r from './noun_skateboarder_1294711.png'
 
 export default class SkateSpot extends Component {
   render() {
     return (
       <div>
-        <Link to={`post/${this.props.skatespot._id}`} className="skateSpot">
-          {this.props.text}
+        <Link to={`post/${this.props.skatespot._id}`}>
+          <img src={sk8r} alt="SK8" className="skateSpot" />
+          {/* {this.props.text} */}
         </Link>
       </div>
     )
